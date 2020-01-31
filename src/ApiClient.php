62075b3d049fa6aed2fadf1ac34fdf90d478e591
Christer Edvartsen <christer.edvartsen@nav.no>
2020-01-31 11:36:50 +0100

    Initial commit of the client and test

---
+++ src/ApiClient.php
@@ -0,0 +1,246 @@
+<?php declare(strict_types=1);
+namespace NAVIT\AzureAd;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\ClientException;
+
+class ApiClient {
+    /**
+     * @var HttpClient
+     */
+    private $httpClient;
+
+    /**
+     * @var string
+     */
+    private $baseUri = 'https://graph.microsoft.com/beta/';
+
+    /**
+     * Class constructor
+     *
+     * @param string $clientId Client ID
+     * @param string $clientSecret Client secret
+     * @param HttpClient $authClient Pre-configured HTTP client for auth
+     * @param HttpClient $httpClient Pre-configured HTTP client for the API calls
+     */
+    public function __construct(string $clientId, string $clientSecret, HttpClient $authClient = null, HttpClient $httpClient = null) {
+        $response = ($authClient ?: new HttpClient())->post('https://login.microsoftonline.com/nav.no/oauth2/v2.0/token', [
+            'form_params' => [
+                'client_id'     => $clientId,
+                'client_secret' => $clientSecret,
+                'scope'         => 'https://graph.microsoft.com/.default',
+                'grant_type'    => 'client_credentials',
+            ],
+        ]);
+
+        $response = json_decode((string) $response->getBody(), true);
+
+        $this->httpClient = $httpClient ?: new HttpClient([
+            'base_uri' => $this->baseUri,
+            'headers'  => [
+                'Accept'        => 'application/json',
+                'Authorization' => sprintf('Bearer %s', $response['access_token']),
+            ],
+        ]);
+    }
+
+    /**
+     * Get a group by ID
+     *
+     * @param string $groupId
+     * @return Models\Group|null
+     */
+    public function getGroupById(string $groupId) : ?Models\Group {
+        try {
+            $response = $this->httpClient->get(sprintf('groups/%s', $groupId));
+        } catch (ClientException $e) {
+            return null;
+        }
+
+        return Models\Group::fromApiResponse($response);
+    }
+
+    /**
+     * Get a group by display name
+     *
+     * @param string $displayName
+     * @return Models\Group|null
+     */
+    public function getGroupByDisplayName(string $displayName) : ?Models\Group {
+        try {
+            $response = $this->httpClient->get('groups', [
+                'query' => [
+                    '$filter' => sprintf('displayName eq \'%s\'', $displayName),
+                ],
+            ]);
+        } catch (ClientException $e) {
+            return null;
+        }
+
+        $groups = json_decode($response->getBody()->getContents(), true);
+
+        return !empty($groups['value'])
+            ? Models\Group::fromArray($groups['value'][0])
+            : null;
+    }
+
+    /**
+     * Create a group
+     *
+     * @param string $displayName The name of the group
+     * @param string $description The description of the group
+     * @param string[] $owners List of users to be added as owners
+     * @param string[] $members List of users to be added as members
+     * @throws Exceptions\InvalidArgumentException
+     * @return Models\Group
+     */
+    public function createGroup(string $displayName, string $description, array $owners = [], array $members = []) : Models\Group {
+        $prefixer = function(string $user) : string {
+            return sprintf('%s/users/%s', rtrim($this->baseUri, '/'), $user);
+        };
+
+        try {
+            $response = $this->httpClient->post('groups', [
+                'json' => array_filter([
+                    'displayName'        => $displayName,
+                    'description'        => $description,
+                    'securityEnabled'    => true,
+                    'mailEnabled'        => true,
+                    'mailNickname'       => $displayName,
+                    'groupTypes'         => ['unified'],
+                    'visibility'         => 'Private',
+                    'owners@odata.bind'  => array_map($prefixer, $owners),
+                    'members@odata.bind' => array_map($prefixer, $members),
+                ]),
+            ]);
+        } catch (ClientException $e) {
+            throw new Exceptions\InvalidArgumentException('Unable to create group', $e->getCode(), $e);
+        }
+
+        return Models\Group::fromApiResponse($response);
+    }
+
+    /**
+     * Set group description
+     *
+     * @param string $groupId The ID of the group
+     * @param string $description The new description
+     * @return bool Returns true on success or false otherwise
+     */
+    public function setGroupDescription(string $groupId, string $description) : bool {
+        try {
+            $this->httpClient->patch(sprintf('groups/%s', $groupId), [
+                'json' => [
+                    'description' => $description,
+                ],
+            ]);
+        } catch (ClientException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Add Azure AD group to an enterprise application
+     *
+     * @param string $groupId The ID of the group to add
+     * @param string $applicationObjectId The object ID of the application to add the group to
+     * @param string $applicationRoleId The role ID the group will receive
+     * @throws InvalidArgumentException
+     * @return void
+     */
+    public function addGroupToEnterpriseApp(string $groupId, string $applicationObjectId, string $applicationRoleId) : void {
+        try {
+            $this->httpClient->post(sprintf('servicePrincipals/%s/appRoleAssignments', $applicationObjectId), [
+                'json' => [
+                    'principalId' => $groupId,
+                    'appRoleId'   => $applicationRoleId,
+                    'resourceId'  => $applicationObjectId,
+                ],
+            ]);
+        } catch(ClientException $e) {
+            throw new Exceptions\InvalidArgumentException('Unable to add group to enterprise application', $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Get all groups connected to a specific enterprise application in Azure AD
+     *
+     * @param string $applicationObjectId The object ID of the application
+     * @return Models\Group[]
+     */
+    public function getEnterpriseAppGroups(string $applicationObjectId) : array {
+        $url = sprintf('servicePrincipals/%s/appRoleAssignedTo', $applicationObjectId);
+
+        return array_filter(array_map(function(array $group) : ?Models\Group {
+            return $this->getGroupById($group['principalId']);
+        }, array_filter($this->getPaginatedData($url, ['principalId', 'principalType']), function(array $group) : bool {
+            return 'group' === strtolower($group['principalType']);
+        })));
+    }
+
+    /**
+     * Get all members in a group
+     *
+     * @param string $groupId The group ID
+     * @return Models\GroupMember[] Returns an array of users
+     */
+    public function getGroupMembers(string $groupId) : array {
+        return array_filter(array_map(function(array $member) : ?Models\GroupMember {
+            try {
+                return Models\GroupMember::fromArray($member);
+            } catch (Exceptions\InvalidArgumentException $e) {
+                return null;
+            }
+        }, $this->getPaginatedData(sprintf('groups/%s/members', $groupId), ['id', 'displayName', 'mail'])));
+    }
+
+    /**
+     * Get all owners of a group
+     *
+     * @param string $groupId The group ID
+     * @return Models\GroupOwnerr[] Returns an array of users
+     */
+    public function getGroupOwners(string $groupId) : array {
+        return array_filter(array_map(function(array $member) : ?Models\GroupOwner {
+            try {
+                return Models\GroupOwner::fromArray($member);
+            } catch (Exceptions\InvalidArgumentException $e) {
+                return null;
+            }
+        }, $this->getPaginatedData(sprintf('groups/%s/owners', $groupId), ['id', 'displayName', 'mail'])));
+    }
+
+    /**
+     * Get paginated data from the API
+     *
+     * @param string $url The URL to fetch
+     * @param array $fields Fields to fetch
+     * @throws Exceptions\RuntimeException
+     * @return array
+     */
+    private function getPaginatedData(string $url, array $fields = []) : array {
+        $entries = [];
+
+        $query = array_filter([
+            '$select' => join(',', $fields),
+            '$top'    => 100
+        ]);
+
+        while ($url) {
+            try {
+                $response = $this->httpClient->get($url, ['query' => $query]);
+            } catch (ClientException $e) {
+                throw new Exceptions\RuntimeException('Unable to fetch paginated data', $e->getCode(), $e);
+            }
+
+            $body = json_decode($response->getBody()->getContents(), true);
+            $entries = array_merge($entries, $body['value']);
+            $url = $body['@odata.nextLink'] ?? null;
+            $query = []; // Only need this for the first request
+        }
+
+        return $entries;
+    }
+}
---
+++ tests/ApiClientTest.php
@@ -0,0 +1,342 @@
+<?php declare(strict_types=1);
+namespace NAVIT\AzureAd;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Middleware;
+
+/**
+ * @coversDefaultClass NAVIT\AzureAd\ApiClient
+ */
+class ApiClientTest extends TestCase {
+    private function getMockClient(array $responses, array &$history = []) : HttpClient {
+        $handler = HandlerStack::create(new MockHandler($responses));
+        $handler->push(Middleware::history($history));
+
+        return new HttpClient(['handler' => $handler]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCanConstructClient() : void {
+        $clientHistory = [];
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')],
+            $clientHistory
+        );
+
+        new ApiClient($id = 'id', $secret = 'secret', $authClient);
+
+        $this->assertCount(1, $clientHistory, 'Missing request');
+        $request = $clientHistory[0]['request'];
+        parse_str($request->getBody()->getContents(), $body);
+
+        $this->assertSame('POST', $request->getMethod());
+        $this->assertSame($id, $body['client_id']);
+        $this->assertSame($secret, $body['client_secret']);
+    }
+
+    /**
+     * @covers ::getGroupById
+     */
+    public function testCanGetGroupById() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [new Response(200, [], '{"id": "some-id", "displayName": "some-display-name", "description": "some description", "mail": "mail@nav.no"}')],
+            $clientHistory
+        );
+
+        $aadGroup = (new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupById('some-id');
+
+        $this->assertCount(1, $clientHistory, 'Expected one request');
+        $this->assertInstanceOf(Models\Group::class, $aadGroup);
+        $this->assertStringEndsWith('groups/some-id', (string) $clientHistory[0]['request']->getUri());
+    }
+
+    /**
+     * @covers ::getGroupById
+     */
+    public function testReturnsNullWhenGroupByIdRequestFails() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $httpClient = $this->getMockClient(
+            [new Response(404)]
+        );
+
+        $this->assertNull((new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupById('some-id'));
+    }
+
+    /**
+     * @covers ::getGroupByDisplayName
+     */
+    public function testCanGetGroupByDisplayName() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [new Response(200, [], '{"value": [{"id": "some-id", "displayName": "some-display-name", "description": "some description", "mail": "mail@nav.no"}]}')],
+            $clientHistory
+        );
+
+        $aadGroup = (new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupByDisplayName('some-display-name');
+
+        $this->assertCount(1, $clientHistory, 'Expected one request');
+        $this->assertInstanceOf(Models\Group::class, $aadGroup);
+        $this->assertStringContainsString('filter=displayName%20eq%20%27some-display-name%27', $clientHistory[0]['request']->getUri()->getQuery());
+    }
+
+    /**
+     * @covers ::getGroupByDisplayName
+     */
+    public function testReturnsNullWhenGroupByNameRequestFails() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $httpClient = $this->getMockClient(
+            [new Response(404)]
+        );
+
+        $this->assertNull((new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupByDisplayName('some display name'));
+    }
+
+    /**
+     * @covers ::getGroupByDisplayName
+     */
+    public function testReturnsNullWhenGroupDoesNotExist() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [new Response(200, [], '{"value": []}')],
+            $clientHistory
+        );
+
+        $this->assertNull((new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupByDisplayName('some-display-name'));
+        $this->assertCount(1, $clientHistory, 'Expected one request');
+        $this->assertStringContainsString('filter=displayName%20eq%20%27some-display-name%27', $clientHistory[0]['request']->getUri()->getQuery());
+    }
+
+    /**
+     * @covers ::createGroup
+     */
+    public function testCanCreateGroup() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [new Response(201, [], '{"id": "some-id", "displayName": "some-display-name", "description": "some description", "mail": "mail@nav.no"}')],
+            $clientHistory
+        );
+
+        $aadGroup = (new ApiClient('id', 'secret', $authClient, $httpClient))->createGroup(
+            'group name',
+            'group description',
+            ['Owner1@nav.no']
+        );
+
+        $this->assertInstanceOf(Models\Group::class, $aadGroup);
+        $this->assertCount(1, $clientHistory, 'Expected one request');
+
+        $request = $clientHistory[0]['request'];
+        $this->assertSame('POST', $request->getMethod());
+
+        $body = json_decode($request->getBody()->getContents(), true);
+
+        $this->assertSame('group name', $body['displayName'], 'Group name not correct');
+        $this->assertStringStartsWith('group description', $body['description'], 'Group description not correct');
+        $this->assertSame('group name', $body['mailNickname'], 'Mail not correct');
+        $this->assertArrayHasKey('owners@odata.bind', $body, 'Missing owners list');
+        $this->assertSame(['https://graph.microsoft.com/beta/users/Owner1@nav.no'], $body['owners@odata.bind'], 'Invalid owners list');
+        $this->assertArrayNotHasKey('members@odata.bind', $body, 'Members list should not be present');
+        $this->assertTrue($body['securityEnabled'], 'securityEnable flag not correct');
+        $this->assertTrue($body['mailEnabled'], 'mailEnabled flag not correct');
+    }
+
+    /**
+     * @covers ::addGroupToEnterpriseApp
+     */
+    public function testCanAddGroupToEnterpriseApp() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [new Response()],
+            $clientHistory
+        );
+
+        (new ApiClient('id', 'secret', $authClient, $httpClient))->addGroupToEnterpriseApp('group-id', 'app-object-id', 'app-role-id');
+
+        $this->assertCount(1, $clientHistory, 'Expected one request');
+
+        $request = $clientHistory[0]['request'];
+        $this->assertSame('POST', $request->getMethod());
+        $this->assertSame('servicePrincipals/app-object-id/appRoleAssignments', (string) $request->getUri());
+        $this->assertSame([
+            'principalId' => 'group-id',
+            'appRoleId' => 'app-role-id',
+            'resourceId' => 'app-object-id'
+        ], json_decode($request->getBody()->getContents(), true), 'Incorrect request body');
+    }
+
+    /**
+     * @covers ::getEnterpriseAppGroups
+     * @covers ::getPaginatedData
+     */
+    public function testCanGetEnterpriseAppGroups() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    '@odata.nextLink' => 'next-link',
+                    'value' => [['principalId' => 'first-id', 'principalType' => 'Group']],
+                ])),
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    '@odata.nextLink' => 'next-link',
+                    'value' => [['principalId' => 'second-id', 'principalType' => 'Group']],
+                ])),
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    'value' => [['principalId' => 'third-id', 'principalType' => 'User']],
+                ])),
+                new Response(200, [], json_encode([
+                    'id'          => 'first-id',
+                    'displayName' => 'first-group',
+                    'description' => 'first description',
+                    'mail'        => 'first@nav.no',
+                ])),
+                new Response(200, [], json_encode([
+                    'id'          => 'second-id',
+                    'displayName' => 'second-group',
+                    'description' => 'second description',
+                    'mail'        => 'second@nav.no',
+                ])),
+            ],
+            $clientHistory
+        );
+
+        $groups = (new ApiClient('id', 'secret', $authClient, $httpClient))->getEnterpriseAppGroups('app-object-id');
+        $this->assertCount(2, $groups);
+        $this->assertCount(5, $clientHistory);
+        $this->assertSame('servicePrincipals/app-object-id/appRoleAssignedTo?%24select=principalId%2CprincipalType&%24top=100', (string) $clientHistory[0]['request']->getUri());
+        $this->assertSame('next-link', (string) $clientHistory[1]['request']->getUri());
+        $this->assertSame('next-link', (string) $clientHistory[2]['request']->getUri());
+        $this->assertSame('groups/first-id', (string) $clientHistory[3]['request']->getUri());
+        $this->assertSame('groups/second-id', (string) $clientHistory[4]['request']->getUri());
+    }
+
+    /**
+     * @covers ::setGroupDescription
+     */
+    public function testCanSetGroupDescription() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient([new Response(200)], $clientHistory);
+
+        $this->assertTrue((new ApiClient('id', 'secret', $authClient, $httpClient))->setGroupDescription('group-id', 'description'));
+        $this->assertCount(1, $clientHistory);
+        $this->assertSame('groups/group-id', (string) $clientHistory[0]['request']->getUri());
+        $this->assertSame('{"description":"description"}', (string) $clientHistory[0]['request']->getBody());
+    }
+
+    /**
+     * @covers ::setGroupDescription
+     */
+    public function testSettingGroupDescriptionReturnsFalseOnFailure() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient([new Response(404)], $clientHistory);
+
+        $this->assertFalse((new ApiClient('id', 'secret', $authClient, $httpClient))->setGroupDescription('group-id', 'description'));
+        $this->assertCount(1, $clientHistory);
+        $this->assertSame('groups/group-id', (string) $clientHistory[0]['request']->getUri());
+    }
+
+    /**
+     * @covers ::getGroupMembers
+     * @covers ::getPaginatedData
+     */
+    public function testCanGetGroupMembers() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    '@odata.nextLink' => 'next-link',
+                    'value' => [['id' => 'first-id', 'displayName' => 'Name 1', 'mail' => 'mail1@nav.no']],
+                ])),
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    'value' => [
+                        ['id' => 'second-id', 'displayName' => 'Name 2', 'mail' => 'mail2@nav.no'],
+                        ['id' => 'third-id', 'displayName' => 'Name 3'], // incomplete, will trigger error internally
+                    ],
+                ])),
+            ],
+            $clientHistory
+        );
+
+        $members = (new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupMembers('group-id');
+        $this->assertCount(2, $members);
+        $this->assertCount(2, $clientHistory);
+        $this->assertSame('groups/group-id/members?%24select=id%2CdisplayName%2Cmail&%24top=100', (string) $clientHistory[0]['request']->getUri());
+        $this->assertSame('next-link', (string) $clientHistory[1]['request']->getUri());
+    }
+
+    /**
+     * @covers ::getGroupOwners
+     * @covers ::getPaginatedData
+     */
+    public function testCanGetGroupOwners() : void {
+        $authClient = $this->getMockClient(
+            [new Response(200, [], '{"access_token": "some secret token"}')]
+        );
+        $clientHistory = [];
+        $httpClient = $this->getMockClient(
+            [
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    '@odata.nextLink' => 'next-link',
+                    'value' => [['id' => 'first-id', 'displayName' => 'Name 1', 'mail' => 'mail1@nav.no']],
+                ])),
+                new Response(200, [], json_encode([
+                    '@odata.context' => 'context-url',
+                    'value' => [
+                        ['id' => 'second-id', 'displayName' => 'Name 2', 'mail' => 'mail2@nav.no'],
+                        ['id' => 'third-id', 'displayName' => 'Name 3'], // incomplete, will trigger error internally
+                    ],
+                ])),
+            ],
+            $clientHistory
+        );
+
+        $owners = (new ApiClient('id', 'secret', $authClient, $httpClient))->getGroupOwners('group-id');
+        $this->assertCount(2, $owners);
+        $this->assertCount(2, $clientHistory);
+        $this->assertSame('groups/group-id/owners?%24select=id%2CdisplayName%2Cmail&%24top=100', (string) $clientHistory[0]['request']->getUri());
+        $this->assertSame('next-link', (string) $clientHistory[1]['request']->getUri());
+    }
+}
